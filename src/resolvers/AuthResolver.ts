1c5cb83fc08538005b84431068d7ec755ec5d212
Samathingamajig <samathingamajig@gmail.com>
2021-05-31 22:29:06 -0500

    OAuth login, and better login/logout responses

--- src/resolvers/AuthResolver.ts
+++ src/resolvers/AuthResolver.ts
@@ -1,22 +1,30 @@
 import { Arg, Ctx, Mutation, Resolver, Query } from 'type-graphql';
+import { getBoltApp } from '../boltApp';
 import { MyContext } from '../graphql-types/MyContext';
 import { UserResponse } from '../graphql-types/UserResponse';
 
 @Resolver()
 export class AuthResolver {
     @Mutation(() => Boolean)
-    async login(@Arg('slackId', () => String) slackId: string, @Ctx() ctx: MyContext): Promise<Boolean> {
-        if (ctx.req.session.slackId) return false;
-
-        ctx.req.session.slackId = slackId;
+    async login(@Arg('slackCode', () => String) slackCode: string, @Ctx() ctx: MyContext): Promise<Boolean> {
+        const boltApp = getBoltApp();
+        try {
+            const authRes = await boltApp.client.oauth.v2.access({
+                client_id: process.env['SLACK_CLIENT_ID'] as string,
+                client_secret: process.env['SLACK_CLIENT_SECRET'] as string,
+                code: slackCode,
+            });
+            if (!authRes.ok) return false;
+            ctx.req.session.slackId = (authRes as any).authed_user.id;
+        } catch (err) {
+            return false;
+        }
 
         return true;
     }
 
     @Mutation(() => Boolean)
     async logout(@Ctx() ctx: MyContext): Promise<Boolean> {
-        if (!ctx.req.session.slackId) return false;
-
         ctx.req.session.slackId = null;
 
         return true;
